29e3507ab4dc8691817b672cb24709d640737614
Peter <mileshko.petr@yandex.ru>
2024-03-28 15:40:42 +0300

    Update header.php
    закрыл индексацию страниц

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name='robots' content='noindex, nofollow'>
 	<title>Бизнес процессы</title>
 	<link rel="stylesheet" href="<?= WORKPRO_STYLES ?>?ver=<?=WORKPRO_VERSION?>">
 </head>
