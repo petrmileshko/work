144549273f1ff3f2aa2010e2941698a9c05bbdb5
Peter <mileshko.petr@yandex.ru>
2024-03-29 11:28:04 +0300

    Create Admin.php
    Добавлена модель для админа

---
+++ inc/Models/Admin.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!class_exists('Admin')) :
+
+	final class Admin extends Model
+	{
+
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function render()
+		{
+			$this->header();
+			parent::render();
+			$this->footer();
+		}
+
+		protected function header($name = '', $args = [])
+		{
+			parent::header($name, $args);
+		}
+
+		protected function footer($name = '', $args = [])
+		{
+			parent::footer($name, $args);
+		}
+	}
+
+endif;
