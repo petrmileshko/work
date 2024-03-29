33a2355a6e96b50f9458943cdafc2eb208c4fd3a
Peter <mileshko.petr@yandex.ru>
2024-03-29 11:13:01 +0300

    Update functions.php
    Подключение модулей

--- functions.php
+++ functions.php
@@ -24,5 +24,7 @@ if (!function_exists('workpro_setup') && WORKPRO) :
 	}
 
 	add_action('after_setup_theme', 'workpro_setup');
+	require_once 'inc/Model.php';
+	require_once 'inc/Models/Login.php';
 	require_once 'inc/Router.php';
 endif;
