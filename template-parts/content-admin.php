174a46f9197d70d0e77948e41a1e75d1c2b5616f
Peter <mileshko.petr@yandex.ru>
2024-04-01 18:16:02 +0300

    Update content-admin.php

--- template-parts/content-admin.php
+++ template-parts/content-admin.php
@@ -1,12 +1,12 @@
-<? if (isset($args) && is_array($args) && !empty($args)) : ?>
+<? if (isset($args) && is_array($args) && !empty($args)) :
+	$lk = new Profile($args);
+	if ($lk) {
+		$lk->render();
+	} ?>
 	<p>
 		Администратор:&nbsp;
 		<?= $args['user_name'] ? $args['user_name'] : 'Неизвестный' ?>
 	</p>
 
 <?
-	$lk = new Profile($args);
-	if ($lk) {
-		$lk->render();
-	}
 endif; ?>
