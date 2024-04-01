c8eac15ce656e7cecc0ffc8a5fb3af81a9db1304
Peter <mileshko.petr@yandex.ru>
2024-04-01 17:18:51 +0300

    Update content-manager.php
    изменил аргумент в шаблоне

--- template-parts/content-manager.php
+++ template-parts/content-manager.php
@@ -1,7 +1,7 @@
 <? if (isset($args) && is_array($args) && !empty($args)) : ?>
 	<p>
 		Менеджер:&nbsp;
-		<?= $args['user_name'] ? $args['user_name'] : 'Неизвестный' ?>
+		<?= $args['user_id'] ? $args['user_id'] : 'Неизвестный' ?>
 	</p>
 <? endif;
 ?>
