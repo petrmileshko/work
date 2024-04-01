68ec20d5a7dc68f59f300c6d629c4d312d8a4f9e
Peter <mileshko.petr@yandex.ru>
2024-04-01 17:23:38 +0300

    Update functions.php
    Подключение модуля Profile

--- functions.php
+++ functions.php
@@ -29,5 +29,6 @@ if (!function_exists('workpro_setup') && WORKPRO) :
 	require_once 'inc/Models/Login.php';
 	require_once 'inc/Models/Manager.php';
 	require_once 'inc/Models/Admin.php';
+	require_once 'inc/Models/Profile.php';
 	require_once 'inc/Router.php';
 endif;
