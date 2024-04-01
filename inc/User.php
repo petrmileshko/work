d4fb965b12b0855f79e2840aea98aed265369a43
Peter <mileshko.petr@yandex.ru>
2024-04-01 12:19:19 +0300

    Update User.php
    Изменения обработчики событий

--- inc/User.php
+++ inc/User.php
@@ -5,7 +5,7 @@ if (!class_exists('User')) :
 	class User
 	{
 		private $user;
-		private $args = [];
+		private $args;
 
 		public static function init()
 		{
@@ -14,19 +14,33 @@ if (!class_exists('User')) :
 				return self::$user;
 			}
 
-			if (self::isLoginEvent()) self::$args = self::authorise();
+			if (self::isLoginEvent()) {
+				self::$args = self::authorise();
+				return self::$user;
+			}
 
+			if (self::isManagerEvent()) {
+				self::$args = ['result' => true, 'message' => 'Отчеты'];
+				return self::$user;
+			}
+
+			self::$args = null;
 			return self::$user;
 		}
 
 		private function authorise()
 		{
-			 return ['result' => true, 'message' => 'Авторизация'];
+			return ['result' => true, 'message' => 'Авторизация'];
 		}
 
 		private function isLoginEvent()
 		{
-			return isset($_POST['form']) && $_POST['form'] === 'Login';
+			return isset($_POST['form']) && $_POST['form'] === 'login';
+		}
+
+		private function isManagerEvent()
+		{
+			return isset($_POST['form']) && $_POST['form'] === 'manager';
 		}
 	}
 
