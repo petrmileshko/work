46b65fe78ad341e0ee712a70fadb240520bb603b
Peter <mileshko.petr@yandex.ru>
2024-04-01 17:46:29 +0300

    Update content-manager.php
    изменения шаблона

--- template-parts/content-manager.php
+++ template-parts/content-manager.php
@@ -1,12 +1,13 @@
-<? if (isset($args) && is_array($args) && !empty($args)) : ?>
+<? if (isset($args) && is_array($args) && !empty($args)) :
+
+	$lk = new Profile($args);
+	if ($lk) {
+		$lk->render();
+	} ?>
 	<main class="page__main manager" id="reports">
 		Менеджер:&nbsp;
 		<?= $args['user_id'] ? $args['user_id'] : 'Неизвестный' ?>
 	</main>
 <?
-	$lk = new Profile($args);
-	if ($lk) {
-		$lk->render();
-	}
 endif;
 ?>
