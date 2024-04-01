c3956d19b715b04d03db88504b29871b4dc19a8c
Peter <mileshko.petr@yandex.ru>
2024-04-01 17:38:10 +0300

    Update header-authorized.php
    Изменение меню в шапке

--- header-authorized.php
+++ header-authorized.php
@@ -1,23 +1,23 @@
 <header class="page__header" id="top">
-<nav class="navigation center-fixed">
-      <div class="navigation__wrapper">
+	<nav class="navigation center-fixed">
+		<div class="navigation__wrapper">
 
-				<?php
-				if (has_custom_logo()) echo get_custom_logo();
-				else echo "Логотип";
-				?>
+			<?php
+			if (has_custom_logo()) echo get_custom_logo();
+			else echo "Логотип";
+			?>
 
 
-        <button class="navigation__button button burger burger--nojs" type="button">
-          <span class="burger__icon">
-            <span class="button__text text visually-hidden">Меню</span>
-          </span>
-        </button>
-      </div>
+			<button class="navigation__button button burger burger--nojs" type="button">
+				<span class="burger__icon">
+					<span class="button__text text visually-hidden">Меню</span>
+				</span>
+			</button>
+		</div>
 
-      <ul class="navigation__menu menu menu--nojs">
-        <li class="menu__item"><a class="menu__link menu__link--current text" href="#">Отчеты</a></li>
-        <li class="menu__item"><a class="menu__link text" href="/wp-admin/profile.php">ЛК</a></li>
-      </ul>
-    </nav>
+		<ul class="navigation__menu menu menu--nojs">
+			<li class="menu__item"><a class="menu__link menu__link--current text" href="#reports">Отчеты</a></li>
+			<li class="menu__item"><a class="menu__link text" href="#lk">ЛК</a></li>
+		</ul>
+	</nav>
 </header>
