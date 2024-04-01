5367f84ff7b23503886774a5a57c74cea3ae992c
Peter <mileshko.petr@yandex.ru>
2024-04-01 17:34:54 +0300

    Update content-profile.php
    Изменение шаблона

--- template-parts/content-profile.php
+++ template-parts/content-profile.php
@@ -1,18 +1,20 @@
 <? if (isset($args) && is_array($args) && !empty($args)) : ?>
-	<section class="profile">
-		<p>
-			Логин:&nbsp;<?= $args['user_login'] ? $args['user_login'] : 'Неизвестно' ?>
-		</p>
-		<p>
-			Имя:&nbsp;<?= $args['user_name'] ? $args['user_name'] : 'Неизвестно' ?>
-		</p>
-		<p>
-			Почта:&nbsp;<?= $args['user_email'] ? $args['user_email'] : 'Неизвестно' ?>
-		</p>
-	</section>
-	<form action="" method="post" autocomplete="off">
-		<button class="button" type="submit">Выход</button>
-		<input type="hidden" name="form" value="logout">
-	</form>
+	<main class="page__main profile" id="lk">
+		<div>
+			<p>
+				Логин:&nbsp;<?= $args['user_login'] ? $args['user_login'] : 'Неизвестно' ?>
+			</p>
+			<p>
+				Имя:&nbsp;<?= $args['user_name'] ? $args['user_name'] : 'Неизвестно' ?>
+			</p>
+			<p>
+				Почта:&nbsp;<?= $args['user_email'] ? $args['user_email'] : 'Неизвестно' ?>
+			</p>
+		</div>
+		<form action="" method="post" autocomplete="off">
+			<button class="button" type="submit">Выход</button>
+			<input type="hidden" name="form" value="logout">
+		</form>
+	</main>
 <? endif;
 ?>
