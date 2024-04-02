d680ba5a5a8e6c4b18c3aaf3f6c49a8ff450aa46
Peter <mileshko.petr@yandex.ru>
2024-04-02 18:09:52 +0300

    Update content-profile.php
    Объявление классов элементов

--- template-parts/content-profile.php
+++ template-parts/content-profile.php
@@ -1,18 +1,20 @@
 <? if (isset($args) && is_array($args) && !empty($args)) : ?>
 	<main class="page__main profile" id="lk">
-		<div>
-			<p>
+		<div class="profile__wrapper">
+			<p class="profile__data">
 				Логин:&nbsp;<?= $args['user_login'] ? $args['user_login'] : 'Неизвестно' ?>
 			</p>
-			<p>
+			<p class="profile__data">
 				Имя:&nbsp;<?= $args['user_name'] ? $args['user_name'] : 'Неизвестно' ?>
 			</p>
-			<p>
+			<p class="profile__data">
 				Почта:&nbsp;<?= $args['user_email'] ? $args['user_email'] : 'Неизвестно' ?>
 			</p>
 		</div>
-		<form action="" method="post" autocomplete="off">
-			<button class="button" type="submit">Выход</button>
+		<form class="form" action="" method="post" autocomplete="off">
+			<button class="form__submit button button--close" type="submit">
+				<span class="button__text text text--hidden">Выход</span>
+			</button>
 			<input type="hidden" name="form" value="logout">
 		</form>
 	</main>
