c13e39e5012750195686f3da4474986962deb07d
Peter <mileshko.petr@yandex.ru>
2024-04-03 11:58:50 +0300

    Update functions.php
    Добавлена инициализация таблиц БД приложения

--- functions.php
+++ functions.php
@@ -2,6 +2,29 @@
 //Инициализация настроек
 require_once 'inc/config.php';
 
+if (!function_exists('workpro_dbase_setup')) :
+
+	function workpro_dbase_setup()
+	{
+
+		if (DBASE_VER === 0) {
+
+			global $wpdb;
+			$wpdb->workpro_reports = $wpdb->prefix . 'workpro_reports';
+			$wpdb->workpro_outlets = $wpdb->prefix . 'workpro_outlets';
+			$query = "
+			CREATE TABLE `" . $wpdb->workpro_reports . "` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `outlets_id` INT NOT NULL , `revenue` FLOAT NOT NULL , `user_id` INT NOT NULL , `report_date` DATE NOT NULL , PRIMARY KEY (`id`), INDEX (`user_id`), INDEX `report_author` (`user_id`)) ENGINE = InnoDB;
+			CREATE TABLE `" . $wpdb->workpro_outlets . "` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `outlets_name` VARCHAR(150) NOT NULL , `outlets_address` MEDIUMTEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+			";
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta($query);
+			update_option('workpro_dbase_version', '1.0');
+		}
+	}
+
+	add_action('init', 'workpro_dbase_setup', 0);
+endif;
+
 if (!function_exists('workpro_setup') && WORKPRO) :
 
 	function workpro_setup()
@@ -22,7 +45,6 @@ if (!function_exists('workpro_setup') && WORKPRO) :
 			'html5'
 		);
 	}
-
 	add_action('after_setup_theme', 'workpro_setup');
 	require_once 'inc/User.php';
 	require_once 'inc/Model.php';
