e027ea81335bf3a4e09169a8f2d5353b647ebddf
Peter <mileshko.petr@yandex.ru>
2024-04-03 12:42:42 +0300

    Update functions.php
    Модификация функции инициализации таблиц

--- functions.php
+++ functions.php
@@ -2,24 +2,20 @@
 //Инициализация настроек
 require_once 'inc/config.php';
 
-if (!function_exists('workpro_dbase_setup')) :
+if (!function_exists('workpro_dbase_setup') && DBASE_VER === 0) :
 
 	function workpro_dbase_setup()
 	{
-
-		if (DBASE_VER === 0) {
-
-			global $wpdb;
-			$wpdb->workpro_reports = $wpdb->prefix . 'workpro_reports';
-			$wpdb->workpro_outlets = $wpdb->prefix . 'workpro_outlets';
-			$query = "
+		global $wpdb;
+		$wpdb->workpro_reports = $wpdb->prefix . 'workpro_reports';
+		$wpdb->workpro_outlets = $wpdb->prefix . 'workpro_outlets';
+		$query = "
 			CREATE TABLE `" . $wpdb->workpro_reports . "` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `outlets_id` INT NOT NULL , `revenue` FLOAT NOT NULL , `user_id` INT NOT NULL , `report_date` DATE NOT NULL , PRIMARY KEY (`id`), INDEX (`user_id`), INDEX `report_author` (`user_id`)) ENGINE = InnoDB;
 			CREATE TABLE `" . $wpdb->workpro_outlets . "` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `outlets_name` VARCHAR(150) NOT NULL , `outlets_address` MEDIUMTEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 			";
-			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-			dbDelta($query);
-			update_option('workpro_dbase_version', '1.0');
-		}
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta($query);
+		update_option('workpro_dbase_version', '1.0');
 	}
 
 	add_action('init', 'workpro_dbase_setup', 0);
