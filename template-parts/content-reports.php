b6739b47dd9146b4ec8ad59b521399eabcb3ecc2
Peter <mileshko.petr@yandex.ru>
2024-04-04 07:48:30 +0300

    Update content-reports.php
    Изменил шаблон отчетов

--- template-parts/content-reports.php
+++ template-parts/content-reports.php
@@ -1,10 +1,10 @@
 <? if (isset($args) && is_array($args) && !empty($args)) : ?>
-	<ul>
+	<ul class="manager__reports reports">
 		<? foreach ($args as $report) : ?>
-			<li>
-				<span><?= convertDate($report['report_date'],'d-m-Y')  ?>&nbsp;&nbsp;</span>
-				<span><?= $report['outlets_address'] ?>&nbsp;&nbsp;</span>
-				<span><?= $report['revenue'] ?> руб: </span>
+			<li class="reports__item report">
+				<time class="report__date" datetime="<?= $report['report_date'] ?>"><?= convertDate($report['report_date'], 'd-m-Y')  ?>&nbsp;&nbsp;</time>
+				<address class="report__outlets"><?= $report['outlets_address'] ?>&nbsp;&nbsp;</address>
+				<span class="report__revenue"><?= $report['revenue'] ?> руб</span>
 			</li>
 		<? endforeach; ?>
 	</ul>
