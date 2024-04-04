2351f9e796f9a9d6e1a7481f1d2486cc1879119f
Peter <mileshko.petr@yandex.ru>
2024-04-04 10:28:41 +0300

    Update content-reportsubmit.php

--- template-parts/content-reportsubmit.php
+++ template-parts/content-reportsubmit.php
@@ -1,8 +1,12 @@
 <? if (isset($args) && is_array($args) && !empty($args)) : ?>
 	<form class="form" action="/" method="post" autocomplete="off">
+	<label>
+		<time class="form__label" datetime="<?=date('Y-m-d h:m:s');?>"><?=date('d-m-Y');?></time>
+		<input type="hidden" name="date" value="<?=date('Y-m-d h:m:s');?>">
+		</label>
 		<label class="form__label">
 		<span>Торговая точка:</span>
-			<select name="outlets_id">
+			<select class="form__select" name="outlets_id">
 				<? foreach($args as $outlets ) :?>
 					<option value="<?=$outlets['id']?>"><?=$outlets['outlets_name']?></option>
 				<? endforeach;?>
@@ -10,7 +14,7 @@
 		</label>
 		<label class="form__label">
 			<span>Выручка:</span>
-			<input type="number" name="revenue" placeholder="Выручка" required>
+			<input class="form__input" type="number" name="revenue" step="0.01" placeholder="Выручка" required>
 		</label>
 		<button class="form__submit button button--add" type="submit">
 			<span class="button__text text text--hidden">Добавить</span>
